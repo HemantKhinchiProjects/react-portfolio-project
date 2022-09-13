54ef04608d12b3c0cabfd1c64d6de19d782d7e04
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-09-13 17:27:44 +0530

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Resume from './components/Resume';
 import ContactUs from './components/ContactUs';
 import resumeData from './resumeData';
 import './style.css';
@@ -8,8 +9,9 @@ import './style.css';
 export default function App() {
   return (
     <div classname="App">
-      <Header />
-      <About />
+      <Header resumeData={resumeData} />
+      <About resumeData={resumeData} />
+      <Resume resumeData={resumeData} />
       <ContactUs resumeData={resumeData} />
     </div>
   );
