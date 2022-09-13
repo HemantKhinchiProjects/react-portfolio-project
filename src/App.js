02c521f890731cf88176754eea5587776d93f63f
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-09-13 16:47:34 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import About from './components/About';
 import './style.css';
 
 export default function App() {
   return (
     <div classname="App">
       <Header />
+      <About />
     </div>
   );
 }
